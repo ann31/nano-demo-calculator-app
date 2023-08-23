9c64ea0046edfab71d25918083af112d06dddb37
Ann Kurian <annkurian31@gmail.com>
2023-08-24 02:40:45 +0530

    Update server.py

--- server.py
+++ server.py
@@ -8,8 +8,9 @@ app.config['SECRET_KEY'] = '163e34c5745bafbebe6e4c88794ab33f'
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
     if(request.method == 'GET'):
-        data = 'hello world!'
-  
+        data = {
+            'code' : 200,
+            'content':'hello world!'}
         return jsonify(data)
     return 'hello world!'
 
